46296236b73e2dba8b0f88becd99031c1b8fcda5
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-09-08 20:52:14 +0100

    Created CheckSymmetry function

--- Assets/Scripts/WFCollapse.cs
+++ Assets/Scripts/WFCollapse.cs
@@ -16,29 +16,72 @@ namespace WFC_Procedural_Generator_Framework
         }
 
 
-
         public void GenerateAdyacencyRules()
         {
             List<Tile> tiles = tileSet.tiles;
+            SortedSet<HashSet<Vector3>> uniqueFaces = new SortedSet<HashSet<Vector3>>();
 
             for (int i = 0; i < tiles.Count; i++)
             {
                 Tile currentTile = tiles[i];
                 Mesh currentMesh = currentTile.mesh;
-                Vector3[] currentVertices = currentMesh.vertices;
+                List<HashSet<Vector3>> faceVertices = GetFaceVertices(currentMesh.vertices);
+
+                for (int j = 0; j < faceVertices.Count; j++)
+                {
+                    string faceID = "";
+
+                    if (!uniqueFaces.Contains(faceVertices[j]))
+                    {
+                        uniqueFaces.Add(faceVertices[j]);
+
+
+                        // check for symmetry 
+                        // if symmetric, add s 
+
+                        // transform all of the vertices to be mirrored 
+                        // add the mirrored version of the face
+                    }
+
+                    //figure what's the faceID and update it 
+                    //update the code for the face 
+                }
 
             }
         }
 
-        public void RandomGenerate()
+        private bool CheckSymmetry(HashSet<Vector3> vertices)
         {
+            List<Vector3> faceVertices = new List<Vector3>(vertices);
+            Vector3 threshold = faceVertices[0] + faceVertices[1];
+            if (threshold.x == 0)
+            {
+                threshold = new Vector3(0, tileSize, tileSize);
+            }
+            else if (threshold.y == 0)
+            {
+                threshold = new Vector3(tileSize, 0, tileSize);
+            }
+            else if (threshold.z == 0)
+            {
+                threshold = new Vector3(tileSize, tileSize, 0);
+            }
+            else
+            {
+                Debug.Log("Something went wrong during check symmetry");
+            }
 
+            for (int i = 0; i < faceVertices.Count; i++)
+            {
+
+            }
+            return false;
         }
 
-        private List<List<Vector3>> GetFaceVertices(Vector3[] vertices)
+        private List<HashSet<Vector3>> GetFaceVertices(Vector3[] vertices)
         {
             int faceCount = 6;
-            List<List<Vector3>> faceVertices = new List<List<Vector3>>(faceCount);
+            List<HashSet<Vector3>> faceVertices = new List<HashSet<Vector3>>(faceCount);
             for (int i = 0; i < vertices.Length; i++)
             {
                 if (vertices[i].x == 0)
@@ -71,5 +114,10 @@ namespace WFC_Procedural_Generator_Framework
             return faceVertices;
         }
 
+
+        public void RandomGenerate()
+        {
+
+        }
     }
 }
