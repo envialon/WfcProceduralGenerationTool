a1caaa71c4b75fd7dd1232ca6df425c407167c48
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-09-16 15:17:07 +0100

    Trying to do an abomination that might work

--- Assets/Scripts/WFCollapse.cs
+++ Assets/Scripts/WFCollapse.cs
@@ -2,6 +2,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public class Comparer : IEqualityComparer<(HashSet<Vector2>, int)>
+{
+    public bool Equals((HashSet<Vector2>, int) x, (HashSet<Vector2>, int) y)
+    {
+        return (x.Item1.SetEquals(y.Item1) && x.Item2 == y.Item2) ? true : false;
+    }
+
+    public int GetHashCode((HashSet<Vector2>, int) obj)
+    {
+        return obj.Item1.GetHashCode() + obj.Item2.GetHashCode();
+    }
+}
 namespace WFC_Procedural_Generator_Framework
 {
     public class WFCollapse : MonoBehaviour
@@ -19,26 +31,28 @@ namespace WFC_Procedural_Generator_Framework
         public void GenerateAdyacencyRules()
         {
             List<Tile> tiles = tileSet.tiles;
-            Dictionary<HashSet<Vector2>, string> uniqueFaces = new Dictionary<HashSet<Vector2>, string>(HashSet<Vector2>.CreateSetComparer());
+            Dictionary<(HashSet<Vector2>, int), string> uniqueFaces = new Dictionary<(HashSet<Vector2>, int), string>(new Comparer());
             int uniqueFaceCounter = 0;
 
             for (int i = 0; i < tiles.Count; i++)
             {
-                List<HashSet<Vector2>> faces = GetFaces(tiles[i].mesh.vertices);
+                List<(HashSet<Vector2>, int)> facesAndTriangleCount = GetFaces(tiles[i].mesh);
 
-                for (int j = 0; j < faces.Count; j++)
+
+                for (int j = 0; j < facesAndTriangleCount.Count; j++)
                 {
+                    (HashSet<Vector2>, int) face = facesAndTriangleCount[j];
                     string faceID = j >= 4 ? "v" : "";
 
-                    if (faces[j].Count == 0)
+                    if (face.Item1.Count == 0)
                     {
                         faceID = "-1";
                     }
-                    else if (!uniqueFaces.ContainsKey(faces[j]))
+                    else if (!uniqueFaces.ContainsKey(face))
                     {
                         faceID = faceID + uniqueFaceCounter;
 
-                        if (CheckFaceSymmetry(faces[j]))
+                        if (CheckFaceSymmetry(face.Item1))
                         {
                             // if symmetric, add s 
                             faceID = faceID + "s";
@@ -47,17 +61,17 @@ namespace WFC_Procedural_Generator_Framework
                         {
                             // transform all of the vertices to be mirrored
                             // add the mirrored version of the face
-                            uniqueFaces.Add(MirrorFace(faces[j]), faceID + "f");
+                            uniqueFaces.Add((MirrorFace(face.Item1),face.Item2),  faceID + "f");
                             // update counter
                             uniqueFaceCounter++;
                         }
-                        uniqueFaces.Add(faces[j], faceID);
+                        uniqueFaces.Add(face, faceID);
                         uniqueFaceCounter++;
                     }
                     else
                     {
                         //figure what's the faceID and update it if it has been registered before
-                        faceID = uniqueFaces[faces[j]];
+                        faceID = uniqueFaces[face];
                     }
                     //update the code for the face 
                     tiles[i].faces[j] = faceID;
@@ -94,42 +108,44 @@ namespace WFC_Procedural_Generator_Framework
             return true;
         }
 
-        private List<HashSet<Vector2>> GetFaces(Vector3[] vertices)
+        private List<(HashSet<Vector2>, int)> GetFaces(Mesh mesh)
         {
             int faceCount = 6;
-            List<HashSet<Vector2>> faceVertices = new List<HashSet<Vector2>>();
+            Vector3[] vertices = mesh.vertices;
+            int[] triangles = mesh.triangles;
+            List<(HashSet<Vector2>, int)> faceVertices = new List<(HashSet<Vector2>, int)>();
             for (int i = 0; i < faceCount; i++)
             {
-                faceVertices.Add(new HashSet<Vector2>());
+                faceVertices.Add(new(new HashSet<Vector2>(), new int()));
             }
 
             for (int i = 0; i < vertices.Length; i++)
             {
                 if (vertices[i].x == 0)
                 {
-                    faceVertices[0].Add(new Vector2(vertices[i].z, vertices[i].y));
+                    faceVertices[0].Item1.Add(new Vector2(vertices[i].z, vertices[i].y));
                 }
                 else if (vertices[i].x == tileSize)
                 {
-                    faceVertices[1].Add(new Vector2(vertices[i].z, vertices[i].y));
+                    faceVertices[1].Item1.Add(new Vector2(vertices[i].z, vertices[i].y));
                 }
 
                 if (vertices[i].z == 0)
                 {
-                    faceVertices[2].Add(new Vector2(vertices[i].x, vertices[i].y));
+                    faceVertices[2].Item1.Add(new Vector2(vertices[i].x, vertices[i].y));
                 }
                 else if (vertices[i].z == tileSize)
                 {
-                    faceVertices[3].Add(new Vector2(vertices[i].x, vertices[i].y));
+                    faceVertices[3].Item1.Add(new Vector2(vertices[i].x, vertices[i].y));
                 }
 
                 if (vertices[i].y == 0)
                 {
-                    faceVertices[4].Add(new Vector2(vertices[i].x, vertices[i].z));
+                    faceVertices[4].Item1.Add(new Vector2(vertices[i].x, vertices[i].z));
                 }
                 else if (vertices[i].y == tileSize)
                 {
-                    faceVertices[5].Add(new Vector2(vertices[i].x, vertices[i].z));
+                    faceVertices[5].Item1.Add(new Vector2(vertices[i].x, vertices[i].z));
                 }
             }
             return faceVertices;
