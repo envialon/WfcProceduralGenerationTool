d4707aaff89faf4e2b9da404434721aacbd386eb
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2023-02-28 12:28:31 +0000

    Adding a TileMap to TilePainter, still doesn't work

--- Assets/Scripts/Interface/TilePainter.cs
+++ Assets/Scripts/Interface/TilePainter.cs
@@ -4,154 +4,157 @@ using UnityEngine;
 using UnityEditor;
 using Unity.VisualScripting;
 using UnityEditor.SearchService;
+using UnityEngine.Tilemaps;
 
-public class TilePainter : MonoBehaviour
+namespace WFC_Procedural_Generator_Framework
 {
-
-    public int numberOfLayers = 1;
-    public int mapSize = 10;
-    public Grid grid;
-
-    [SerializeField]
-    private Material tilemapRenderMaterial;
-
-    public int selectedLayer = 0;
-    private BoxCollider selectedCollider;
-    private GameObject child;
-
-
-
-    public void Initialize()
+    public class TilePainter : MonoBehaviour
     {
-        if (child == null)
+        public int numberOfLayers = 1;
+        public int selectedLayer = 0;
+        public int selectedTile = 1;
+        public int mapSize = 10;
+
+        public Grid grid;
+        public TileSet tileSet;
+
+        [SerializeField]
+        private Material tilemapRenderMaterial;
+
+        private BoxCollider selectedCollider;
+        private GameObject child;
+        private Tilemap tileMap;
+
+
+        public void Initialize()
         {
-            child = new GameObject("GridSelector");
-            child.transform.localPosition = new Vector3(0, 0, 0);
-            child.transform.parent = transform;
+            if (child == null)
+            {
+                child = new GameObject("GridSelector");
+                child.transform.localPosition = new Vector3(0, 0, 0);
+                child.transform.parent = transform;
+            }
+            if (selectedCollider == null)
+            {
+                selectedCollider = child.AddComponent<BoxCollider>();
+                selectedCollider.size = new Vector3(mapSize, 0, mapSize);
+                selectedCollider.center = new Vector3(mapSize / 2, 0, mapSize / 2);
+            }
+            if (grid == null)
+            {
+                grid = child.AddComponent<Grid>();
+                grid.cellSwizzle = GridLayout.CellSwizzle.XZY;
+            }
+            
         }
-        if (selectedCollider == null)
+
+        private void MoveGridSelector()
         {
-            selectedCollider = child.AddComponent<BoxCollider>();
-            selectedCollider.size = new Vector3(mapSize, 0, mapSize);
-            selectedCollider.center = new Vector3(mapSize / 2, 0, mapSize / 2);
+            grid.transform.position = new Vector3(0f, selectedLayer, 0f);
         }
-        if (grid == null)
+
+        public void AddLayer()
         {
-            grid = child.AddComponent<Grid>();
-            grid.cellSwizzle = GridLayout.CellSwizzle.XZY;
+            numberOfLayers++;
+            selectedLayer = numberOfLayers - 1;
+            MoveGridSelector();
+            //GameObject child = new GameObject();
+            //child.transform.parent = transform;
+            //child.transform.position = new Vector3(0f, layers.Count, 0f);
+
+            //child.name = "Layer " + layers.Count;
+
+            //layers.Add(child.AddComponent<Grid>());
+            //layers[layers.Count - 1].cellSwizzle = GridLayout.CellSwizzle.XZY;
+
+            //layers[selectedLayer] = layers[layers.Count - 1];
+        }
+
+        public void HandleClick(Vector3 mousePosition)
+        {
+            //Ray ray = SceneView.lastActiveSceneView.camera.ScreenPointToRay(mousePosition);
+            Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 1000f))
+            {
+                Vector3Int cellPosition = grid.WorldToCell(hit.point);
+                Debug.Log("Hit at: " + hit.point + " Corresponds to cell " + cellPosition);
+            }
+        }
+
+        public void HandleKeyPress(KeyCode keycode)
+        {
+            switch (keycode)
+            {
+                case KeyCode.UpArrow:
+                    selectedLayer = (selectedLayer + 1 >= numberOfLayers) ? numberOfLayers - 1 : selectedLayer++;
+                    MoveGridSelector();
+                    break;
+                case KeyCode.DownArrow:
+                    selectedLayer = (selectedLayer - 1 < 0) ? 0 : selectedLayer--;
+                    MoveGridSelector();
+                    break;
+                case KeyCode.LeftArrow:
+                    selectedTile = (selectedTile - 1 < 0) ? 0 : selectedTile--;
+                    break;
+                case KeyCode.RightArrow:
+                    selectedTile = (selectedTile + 1 >= tileSet.tiles.Count - 1) ? tileSet.tiles.Count - 1 : selectedTile++;
+                    break;
+            }
+        }
+
+        public void SerializeTileMap()
+        {
+            Debug.Log("Not implemented");
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            for (int i = 0; i <= mapSize; i++)
+            {
+                Gizmos.DrawLine(new Vector3(0, selectedLayer, i), new Vector3(mapSize, selectedLayer, i));
+                Gizmos.DrawLine(new Vector3(i, selectedLayer, 0), new Vector3(i, selectedLayer, mapSize));
+            }
         }
     }
 
-    private void MoveGridSelector()
+
+    [CustomEditor(typeof(TilePainter))]
+    public class TilePainterEditor : Editor
     {
-        grid.transform.position = new Vector3(0f, selectedLayer, 0f);
-    }
-
-    public void AddLayer()
-    {
-        numberOfLayers++;
-        selectedLayer = numberOfLayers - 1;
-        MoveGridSelector();
-        //GameObject child = new GameObject();
-        //child.transform.parent = transform;
-        //child.transform.position = new Vector3(0f, layers.Count, 0f);
-
-        //child.name = "Layer " + layers.Count;
-
-        //layers.Add(child.AddComponent<Grid>());
-        //layers[layers.Count - 1].cellSwizzle = GridLayout.CellSwizzle.XZY;
-
-        //layers[selectedLayer] = layers[layers.Count - 1];
-    }
-
-    public void ClearLayers()
-    {
-        foreach (Transform child in transform)
+        public override void OnInspectorGUI()
         {
-            DestroyImmediate(child.gameObject);
+            DrawDefaultInspector();
+            GUILayout.Space(10);
+
+            TilePainter tilePainter = (TilePainter)target;
+            if (GUILayout.Button("Add Layer"))
+            {
+                tilePainter.AddLayer();
+            }
+
+            GUILayout.Space(10);
+            if (GUILayout.Button("Serialize tileMap"))
+            {
+                tilePainter.SerializeTileMap();
+            }
+            tilePainter.Initialize();
+        }
+
+
+        private void OnSceneGUI()
+        {
+            TilePainter tilePainter = (TilePainter)target;
+            Event e = Event.current;
+            if (e.type == EventType.MouseDown)
+            {
+                Debug.Log("Mouse down");
+                tilePainter.HandleClick(e.mousePosition);
+            }
+            if (e.type == EventType.KeyDown)
+            {
+                tilePainter.HandleKeyPress(e.keyCode);
+            }
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
         }
     }
-
-    public void HandleClick(Vector3 mousePosition)
-    {        
-        //Ray ray = SceneView.lastActiveSceneView.camera.ScreenPointToRay(mousePosition);
-        Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, 1000f))
-        {
-            Vector3Int cellPosition = grid.WorldToCell(hit.point);
-            Debug.Log("Hit at: " + hit.point + " Corresponds to cell " + cellPosition);
-        }
-    }
-
-    public void HandleKeyPress(KeyCode keycode)
-    {
-        switch (keycode)
-        {
-            case KeyCode.UpArrow:
-                selectedLayer = (selectedLayer + 1 >= numberOfLayers) ? numberOfLayers - 1 : selectedLayer++;
-                MoveGridSelector();
-                break;
-            case KeyCode.DownArrow:
-                selectedLayer = (selectedLayer - 1 < 0) ? 0 : selectedLayer--;
-                MoveGridSelector();
-                break;
-        }
-    }
-
-    public void SerializeTileMap()
-    {
-
-    }
-    private void OnDrawGizmosSelected()
-    {
-        for (int i = 0; i <= mapSize; i++)
-        {
-            Gizmos.DrawLine(new Vector3(0, selectedLayer, i), new Vector3(mapSize, selectedLayer, i));
-            Gizmos.DrawLine(new Vector3(i, selectedLayer, 0), new Vector3(i, selectedLayer, mapSize));
-        }
-    }
-}
-
-
-[CustomEditor(typeof(TilePainter))]
-public class TilePainterEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-        TilePainter tilePainter = (TilePainter)target;
-        if (GUILayout.Button("Add Layer"))
-        {
-            tilePainter.AddLayer();
-        }
-        if (GUILayout.Button("Clear Layers"))
-        {
-            tilePainter.ClearLayers();
-        }
-        GUILayout.Space(10);
-        if (GUILayout.Button("Serialize tileMap"))
-        {
-            tilePainter.SerializeTileMap();
-        }
-        tilePainter.Initialize();
-    }
-
-
-    private void OnSceneGUI()
-    {
-        TilePainter tilePainter = (TilePainter)target;
-        Event e = Event.current;
-        if (e.type == EventType.MouseDown)
-        {
-            Debug.Log("Mouse down");
-            tilePainter.HandleClick(e.mousePosition);
-        }
-        if (e.type == EventType.KeyDown)
-        {
-            tilePainter.HandleKeyPress(e.keyCode);
-        }
-        HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
-    }
-
-
 }
--- Assets/Scripts/WFC Algorithm/Tiles/TileMap.cs
+++ Assets/Scripts/WFC Algorithm/Tiles/TileMap.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 namespace WFC_Procedural_Generator_Framework
 {
-    public class TileMap 
+    public class TileMap
     {
         public int mapSize = 10;
-        public Tile[,] map;
+        public int height = 1;
+        public Tile[,,] map;
 
-        public TileMap(int mapSize)
+        public TileMap(int mapSize = 10, int height = 1)
         {
             this.mapSize = mapSize;
-            map = new Tile[mapSize, mapSize];
+            this.height = height;
+            map = new Tile[mapSize, height, mapSize];
+        }
+
+        public void SetTile(Tile tile, int x, int y, int z)
+        {
+            if (x >= 0 && y >= 0 && z >= 0 && x < mapSize
+                && z < mapSize && y < height)
+            {
+                map[x, y, z] = tile;
+            }
+        }
+
+        public void RotateAt(int x, int y, int z)
+        {
+            map[x, y, z].RotateClockwise();
         }
     }
 }
