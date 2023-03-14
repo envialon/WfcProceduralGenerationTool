38b20080f3a52454c2c526b98179d89cb8e9b2f3
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2023-03-14 09:29:53 +0000

    Started the neighbouring processing in InputReader

--- Assets/Scripts/WFC Algorithm/WfcGenerator/InputReader.cs
+++ Assets/Scripts/WFC Algorithm/WfcGenerator/InputReader.cs
@@ -1,9 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 
 namespace WFC_Procedural_Generator_Framework
 {
+    public enum Directions
+    {
+        north,
+        south,
+        west,
+        east
+    }
+
     public struct PatternInfo
     {
         public int frecuency;
@@ -11,12 +21,21 @@ namespace WFC_Procedural_Generator_Framework
         public float relativeFrecuencyLog2;
         public int[,,] pattern;
 
+        public Dictionary<Directions, List<int>> neigbourIndices;
+
         public PatternInfo(int[,,] pattern)
         {
             this.pattern = pattern;
             frecuency = 0;
             relativeFrecuency = 0;
             relativeFrecuencyLog2 = 0;
+            neigbourIndices = new Dictionary<Directions, List<int>>
+            {
+                { Directions.north, new List<int>() },
+                { Directions.south, new List<int>() },
+                { Directions.west, new List<int>() },
+                { Directions.east, new List<int>() }
+            };
         }
 
         public static PatternInfo operator ++(PatternInfo patternInfo)
@@ -36,7 +55,6 @@ namespace WFC_Procedural_Generator_Framework
         private int mapSize;
         private int[,,] indexMap;
         private PatternInfo[] patterns;
-
 
         /// <summary>
         /// Deberemos transformar la información de tile y rotación a enteros puramente, acabaremos con 
@@ -102,6 +120,61 @@ namespace WFC_Procedural_Generator_Framework
             }
         }
 
+        private void CheckForNeighbourhood2D(int currentIndex, PatternInfo current, int candidateIndex, PatternInfo candidate)
+        {
+            bool northNeighbour = true;
+            bool southNeighbour = true;
+            bool eastNeighbour = true;
+            bool westNeighbour = true;
+
+            int[,,] currentGrid = current.pattern;
+            int[,,] candidateGrid = candidate.pattern;
+
+            for (int i = 0; i < patternSize; i++)
+            {
+                northNeighbour &= currentGrid[i, 0, patternSize] == candidateGrid[i, 0, 0];
+                southNeighbour &= currentGrid[i, 0, 0] == candidateGrid[i, 0, patternSize];
+                eastNeighbour &= currentGrid[patternSize, 0, i] == candidateGrid[0, 0, i];
+                westNeighbour &= currentGrid[0, 0, i] == candidateGrid[patternSize, 0, i];
+            }
+
+            if (northNeighbour)
+            {
+                candidate.neigbourIndices[Directions.north].Add(currentIndex);
+                current.neigbourIndices[Directions.north].Add(candidateIndex);
+            }
+            if (southNeighbour)
+            {
+                candidate.neigbourIndices[Directions.south].Add(currentIndex);
+                current.neigbourIndices[Directions.south].Add(candidateIndex);
+            }
+            if (eastNeighbour)
+            {
+                candidate.neigbourIndices[Directions.east].Add(currentIndex);
+                current.neigbourIndices[Directions.east].Add(candidateIndex);
+            }
+            if (westNeighbour)
+            {
+                candidate.neigbourIndices[Directions.west].Add(currentIndex);
+                current.neigbourIndices[Directions.west].Add(candidateIndex);
+            }
+        }
+
+        private void PopulatePatternNeighbours()
+        {
+            int numberOfPatterns = patterns.Length;
+            for (int i = 0; i < numberOfPatterns; i++)
+            {
+                PatternInfo current = patterns[i];
+                for (int j = i; j < numberOfPatterns; j++)
+                {
+                    PatternInfo candidate = patterns[i];
+                    CheckForNeighbourhood2D(i, current, j, candidate);
+                }
+            }
+            throw new NotImplementedException();
+        }
+
         public InputReader(InputTileMapData inputTileMap)
         {
             this.inputTileMap = inputTileMap;
@@ -109,8 +182,8 @@ namespace WFC_Procedural_Generator_Framework
             this.height = inputTileMap.height;
             PopulateIndexMap();
             PopulatePatternFrequency2D();
+            PopulatePatternNeighbours();
         }
-
 
     }
 }
